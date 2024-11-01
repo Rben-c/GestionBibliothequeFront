d1b3870507b458afc46855509b4b8cd22a2464c3
Rim <hnebmir@gmail.com>
2024-11-01 22:16:33 +0100

    Activation de la Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 // components/Navbar.js
 import React from 'react';
-import '../styles/Navbar.css'; // CSS spécifique pour ce composant
+import { Link } from 'react-router-dom'; // Importer Link
+import '../styles/Navbar.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Navbar = () => {
@@ -11,43 +12,54 @@ const Navbar = () => {
         <label htmlFor="nav-toggle">
           <span id="nav-toggle-burger"></span>
         </label>
-        
       </div>
 
       <div id="nav-content">
-        <div className="nav-button">
-          <i className="fas fa-user-graduate"></i> {/* Icon for student management */}
-          <span>Gestion des étudiants</span>
-        </div>
-        <div className="nav-button">
-          <i className="fas fa-users"></i> {/* Icon for personnel management */}
-          <span>Gestion du personnel</span>
-        </div>
-        <div className="nav-button">
-          <i className="fas fa-user-shield"></i> {/* Icon for admin management */}
-          <span>Gestion des administrateurs</span>
-        </div>
-        <div className="nav-button">
-          <i className="fas fa-book"></i> {/* Icon for librarian management */}
-          <span>Gestion des bibliothécaires</span>
-        </div>
-        <hr/>
+        <Link to="/gestion-etudiants">
+          <div className="nav-button">
+            <i className="fas fa-user-graduate"></i>
+            <span>Gestion des étudiants</span>
+          </div>
+        </Link>
+        <Link to="/gestion-personnel">
+          <div className="nav-button">
+            <i className="fas fa-users"></i>
+            <span>Gestion du personnel</span>
+          </div>
+        </Link>
+        <Link to="/gestion-Admin">
+          <div className="nav-button">
+            <i className="fas fa-user-shield"></i>
+            <span>Gestion des administrateurs</span>
+          </div>
+        </Link>
+        <Link to="/gestion-bibliothecaires">
+          <div className="nav-button">
+            <i className="fas fa-book"></i>
+            <span>Gestion des bibliothécaires</span>
+          </div>
+        </Link>
+        <hr />
 
-        <div className="nav-button">
-          <i className="fas fa-tachometer-alt"></i> {/* Icon for dashboard */}
-          <span>Tableau de bord</span>
-        </div>
+        <Link to="/tableau-de-bord">
+          <div className="nav-button">
+            <i className="fas fa-tachometer-alt"></i>
+            <span>Tableau de bord</span>
+          </div>
+        </Link>
 
-        <hr/>
+        <hr />
         <div id="nav-content-highlight"></div>
       </div>
 
       <input id="nav-footer-toggle" type="checkbox" />
       <div id="nav-footer">
-        <div className="nav-button">
-          <i className="fas fa-sign-out-alt"></i> {/* Icon for logout */}
-          <span>Déconnexion</span>
-        </div>
+        <Link to="/login">
+          <div className="nav-button">
+            <i className="fas fa-sign-out-alt"></i>
+            <span>Déconnexion</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
